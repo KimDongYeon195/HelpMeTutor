ef130b52dbad74322f2636c820263d8e3ebbf4fe
Austin\AustinKim <dong961125@gmail.com>
2026-04-01 22:43:35 +0900

    [Other][새로운 액터 클래스 생성]
    깃 협업 연습(패치 - 풀)을 위한 액터 클래스 생성

---
+++ Source/HelpMeTutor/MyActor.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+// MyActor.cpp
+
+
+#include "MyActor.h"
+
+AMyActor::AMyActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+
